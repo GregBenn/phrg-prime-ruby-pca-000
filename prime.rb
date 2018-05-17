e8a949a33084d1455b0b78554ced981cc76cca3e
GregBenn <gbenn07@gmail.com>
2018-05-17 16:43:55 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+require 'pry'
+
+def prime?(number)
+  if number < 2
+    false
+  elsif number == 2
+    true
+  else
+    num = (2..number - 1).to_a
+  !(num.each.any? {|n| number % n == 0})
+  end
+end
